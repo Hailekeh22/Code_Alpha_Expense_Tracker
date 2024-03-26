ae52d7cf5dae1cc86ecacdede8887e085d63fc25
Hailekeh22 <hailekjt@gmail.com>
2024-03-26 15:51:26 +0300

    finsihed adding list

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let submit = document.querySelector(".addincome");
+let addincome = document.querySelector(".addincome");
+let addexpence = document.querySelector(".addexpence");
 
-submit.addEventListener("click", () => {
+function updatetotalbalance() {
   let incomescource = document.getElementById("incomesouce").value;
   let incomeamount = document.getElementById("incomeamount").value;
   let totalbalance = document.getElementById("totalbalance");
 
-  if (incomeamount == "" || incomescource == "") {
+  if (incomeamount == "" || incomeamount <= 0 || incomescource == "") {
     alert("Please Enter a value");
   } else {
     amt = parseInt(totalbalance.innerHTML);
@@ -14,5 +15,25 @@ submit.addEventListener("click", () => {
 
     document.getElementById("incomesouce").value = "";
     document.getElementById("incomeamount").value = "";
+    addincomelist(incomescource, incomeamount);
   }
+}
+
+function addincomelist(s, a) {
+  let individualcount = document.querySelector(".individualcount");
+
+  let creteincomelist = document.createElement("div");
+  creteincomelist.classList.add("incomecount");
+  creteincomelist.innerHTML = `<p class="inctitle">${s}</p> <p class="inc">+ ${a}</p>`;
+
+  individualcount.appendChild(creteincomelist);
+}
+
+addincome.addEventListener("click", () => {
+  updatetotalbalance();
 });
+
+function updatetotalexpence() {
+  let expencename = document.getElementById("expencename").value;
+  let expenceamount = document.getElementById("expenceamount").value;
+}
