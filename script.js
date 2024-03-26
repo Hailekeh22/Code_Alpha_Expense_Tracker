553859d90a5afaf78994a188411891e750fa0da5
Hailekeh22 <hailekjt@gmail.com>
2024-03-26 12:24:13 +0300

    update available balnce completed

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
 let submit = document.querySelector(".addincome");
-let incomescource;
-let incomeamount;
-
-function getvalue() {
-  incomescource = document.getElementById("incomesouce").value;
-  incomeamount = document.getElementById("incomeamount").value;
-}
-
-function postvalue() {
-  getvalue();
-  console.log(
-    `Your Source of income is ${incomescource} and amount is ${incomeamount}`
-  );
-
-  document.getElementById("incomesouce").value = "";
-  document.getElementById("incomeamount").value = "";
-}
-
-function updatebalance() {
-  let currentbalance = document.getElementById("totalbalance");
-  let incomeamount = document.getElementById("incomeamount").value;
-
-  let currentbalanceval = parseInt(currentbalance.textContent);
-
-  let updatedbalance = currentbalanceval + incomeamount;
-  console.log(updatedbalance);
-}
 
 submit.addEventListener("click", () => {
-  postvalue();
-  updatebalance();
+  let incomescource = document.getElementById("incomesouce").value;
+  let incomeamount = document.getElementById("incomeamount").value;
+  let totalbalance = document.getElementById("totalbalance");
+
+  if (incomeamount == "" || incomescource == "") {
+    alert("Please Enter a value");
+  } else {
+    amt = parseInt(totalbalance.innerHTML);
+
+    updatedbalance = totalbalance.innerHTML = amt + parseInt(incomeamount);
+
+    document.getElementById("incomesouce").value = "";
+    document.getElementById("incomeamount").value = "";
+  }
 });
